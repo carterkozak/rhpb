ebbd3feca557cca8553e893a82011e7b54ae56f3
ckozak <ckozak@redhat.com>
2014-04-15 10:04:27 -0400

    Added usage and ability to fetch pastebin by url or id to stdout

--- rhpb.py
+++ rhpb.py
@@ -4,6 +4,17 @@ import sys
 import urllib2
 from urllib import urlencode
 
+def print_usage():
+    '''
+    Always print usage to stderr, we never want it piped into an output file
+    '''
+    sys.stderr.write("Usage:\nrhpb reads stdin untill EOF and prints the pastebin link\n" +
+            "  EX: $ cat foo.txt | rhpb\n" +
+            "  http://pastebin.test.redhat.com/{id}\n" +
+            "rhpb will can dump a posted pastebin to stdout given the id or url\n" +
+            "  EX: $ rhpb http://pastebin.test.redhat.com/{id}\n" +
+            "  Hello, World!\n")
+
 def buildData(to_post):
     data = {}
     login = os.getlogin()
@@ -17,8 +28,29 @@ def buildData(to_post):
     data['remember'] = '0'
     return urlencode(data)
 
-encoded = buildData(sys.stdin.read())
+def post():
+    encoded = buildData(sys.stdin.read())
 
-request = urllib2.Request("http://pastebin.test.redhat.com/pastebin.php", data=encoded, headers={'Content-type': 'application/x-www-form-urlencoded'})
-r = urllib2.urlopen(request)
-print r.geturl()
+    request = urllib2.Request("http://pastebin.test.redhat.com/pastebin.php",
+            data=encoded, headers={'Content-type': 'application/x-www-form-urlencoded'})
+    r = urllib2.urlopen(request)
+    print r.geturl()
+
+def fetch():
+    try:
+        arg = str(int(sys.argv[1].split('/')[-1]))
+        f = urllib2.urlopen('http://pastebin.test.redhat.com/pastebin.php?dl=%s' % arg)
+        print f.read()
+        f.close()
+    except:
+        # Could probably do some better exception handling for
+        # network exceptions
+        print_usage()
+
+arglen = len(sys.argv)
+if arglen == 1:
+    post()
+elif arglen == 2:
+    fetch()
+else:
+    print_usage()
